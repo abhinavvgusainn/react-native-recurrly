2e0f0b53a8851b82040a98a0f0d5c5ec6977febc
Abhinav Gusain <232528803+abhinavvgusainn@users.noreply.github.com>
2026-04-08 17:53:02 +0530

    feat: project setup, nativewind, routing, tab navigation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,5 +2,5 @@ import { Stack } from "expo-router";
 import "@/global.css"
 
 export default function RootLayout() {
-  return <Stack screenOptions={{headerShown: false}} initialRouteName="(tabs)"/>;
+  return <Stack screenOptions={{headerShown: false}}/>;
 }
